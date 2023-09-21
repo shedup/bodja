a81c9e56a56908a39f0c4fed0dba93d96d3feeeb
Tenzin <chaksamt@gmail.com>
2023-09-21 00:24:04 -0500

    flex box added to front page

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -6,23 +6,22 @@ function Item(props) {
   return (
     <>
       {props.items.length == 0 && <p>No items sorry.</p>}
-      {props.items.map((key, index) => (
-        <div className="card" key={props.items[index].name}>
-          <img
-            src={tshirt}
-            className="card-img-top"
-            alt="Placeholder"
-            style={{ width: "18rem" }}
-          />
-          <div className="card-body">
-            <h5 className="card-title">{props.items[index].name}</h5>
-            <p className="card-text">{props.items[index].description}</p>
-            <a href="#" className="btn btn-primary">
-              Buy
-            </a>
-          </div>
+      <div className="container-fluid">
+        <div className="row">
+          {props.items.map((key, index) => (
+            <div className="card col-4" key={props.items[index].name}>
+              <img src={tshirt} className="card-img-top" alt="Placeholder" />
+              <div className="card-body">
+                <h5 className="card-title">{props.items[index].name}</h5>
+                <p className="card-text">{props.items[index].description}</p>
+                <a href="#" className="btn btn-primary">
+                  Buy
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
