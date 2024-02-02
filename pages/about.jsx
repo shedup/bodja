be9020ffee0a48a638310244e038ea134c08ae24
Tenzin <chaksamt@gmail.com>
2024-02-01 20:18:33 -0600

    added about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <div className="heading-about">
+        <h1>
+          To Inspire, Connect, and Uplift Tibetan youth in exile with stories
+          that matter
+        </h1>
+        <p>
+          Get ready to be empowered with engaging news and stories that truly
+          matter! Our aim is to inspire, connect and uplift the Tibetan youth
+          through a podcast medium that will leave you feeling motivated and
+          connected!
+        </p>
+      </div>
+      <div className="videos">
+        <iframe
+          width="711"
+          height="400"
+          src="https://www.youtube.com/embed/ZTzZB9Z6hEQ"
+          frameborder="0"
+        ></iframe>
+      </div>
+      <div className="heading-about">
+        <h2>Contact Us</h2>
+        <p>
+          <strong>bodja.podcast@gmail.com</strong>
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- pages/team.jsx
+++ pages/team.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Team = () => {
-  return <div>Team</div>;
+  return (
+    <>
+      <div className="team-container">
+        <h1>Team Members</h1>
+        <p>Coming soon...</p>
+      </div>
+    </>
+  );
 };
 
 export default Team;
