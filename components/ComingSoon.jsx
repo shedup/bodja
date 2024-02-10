87343857dfa14b683107bb7ce34a44f97903f582
Tenzin <chaksamt@gmail.com>
2024-02-09 19:05:40 -0600

    testing stage

--- components/ComingSoon.jsx
+++ components/ComingSoon.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const ComingSoon = () => {
   return (
     <div className="misc-container">
-      <h1>Coming Soon...</h1>
+      <h1>Coming Soon... stage</h1>
       <Link href="/about">
         <button type="button" className="btn-cart">
           Check out our about page
